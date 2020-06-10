ed0fa17473e95e69e915c2ab482e2d31e653aa8a
flpgrz <sendtofilippo@gmail.com>
2020-06-11 00:20:11 +0200

    restructuring the plotter

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -31,69 +31,71 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 import tf
 
 
-mpl.rcParams['legend.fontsize'] = 10
-fig = plt.figure()
-plot3D = fig.add_subplot(111, projection='3d')
+class Plotter:
+    def __init__(self):
+        mpl.rcParams['legend.fontsize'] = 10
+        self.fig = plt.figure()
+        self.plot3D = self.fig.add_subplot(111, projection='3d')
 
-position = np.empty(shape=[0, 3])
-gt_position = np.empty(shape=[0, 3])
-position_dict = {"prediction": position, "gt": gt_position}
+        position = np.empty(shape=[0, 3])
+        gt_position = np.empty(shape=[0, 3])
+        self.position_dict = {'prediction': position, 'gt': gt_position}
 
-orientation = np.empty(shape=[0, 4])
-gt_orientation = np.empty(shape=[0, 4])
-orientation_dict = {"prediction": orientation, "gt": gt_orientation}
+        orientation = np.empty(shape=[0, 4])
+        gt_orientation = np.empty(shape=[0, 4])
+        self.orientation_dict = {'prediction': orientation, 'gt': gt_orientation}
 
-tfListener = tf.TransformListener()
+        self.tfListener = tf.TransformListener()
+
+        self.callback_count = 0
+
+    def odom_callback(self, pwcs):
+        self.callback_count += 1
+
+        (trans, rot) = self.tfListener.lookupTransform('/world', '/imu_link', rospy.Time(0))
+
+        new_position = np.expand_dims(np.array([pwcs.pose.pose.position.x,
+                                                pwcs.pose.pose.position.y,
+                                                pwcs.pose.pose.position.z]), axis=0)
+        self.position_dict['prediction'] = np.append(self.position_dict['prediction'], new_position, axis=0)
+
+        new_orientation = np.expand_dims(np.array([pwcs.pose.pose.orientation.x,
+                                                   pwcs.pose.pose.orientation.y,
+                                                   pwcs.pose.pose.orientation.z,
+                                                   pwcs.pose.pose.orientation.w]), axis=0)
+        self.orientation_dict['prediction'] = np.append(self.orientation_dict['prediction'], new_orientation, axis=0)
+
+        new_gt_position = np.expand_dims(np.array(trans), axis=0)
+        self.position_dict['gt'] = np.append(self.position_dict['gt'], new_gt_position, axis=0)
+
+        new_gt_orientation = np.expand_dims(np.array(rot), axis=0)
+        self.orientation_dict['gt'] = np.append(self.orientation_dict['gt'], new_gt_orientation, axis=0)
+
+        if self.callback_count % 100 == 0:  # only update the plot after 100 callbacks
+            self.plot3D.clear()
+            self.plot3D.set_title('Position')
+            self.plot3D.plot(self.position_dict['prediction'][:, 0],
+                             self.position_dict['prediction'][:, 1],
+                             self.position_dict['prediction'][:, 2], 'c1', label='predicted position')
+            self.plot3D.plot(self.position_dict['gt'][:, 0],
+                             self.position_dict['gt'][:, 1],
+                             self.position_dict['gt'][:, 2], 'r1', label='gt position')
+            self.plot3D.legend()
+
+            #ToDo: plot orientation
 
 
-def plotter():
-    print("Plotter starting...")
+if __name__ == '__main__':
+    print('Plotter node starting...')
     rospy.init_node('plotter', anonymous=True)
+    plotter = Plotter()
 
     while not rospy.is_shutdown():
         try:
-            rospy.Subscriber("kf_odom/odom", PoseWithCovarianceStamped, odom_callback, (position_dict, orientation_dict))
-            plot3D.legend()
+            rospy.Subscriber('kf_odom/odom', PoseWithCovarianceStamped, plotter.odom_callback)
             plt.show()
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-    
+
     rospy.spin()
 
-
-def odom_callback(pwcs, args):
-    (trans, rot) = tfListener.lookupTransform('/world', '/imu_link', rospy.Time(0))
-    
-    new_position = np.expand_dims(np.array([pwcs.pose.pose.position.x,
-                                            pwcs.pose.pose.position.y,
-                                            pwcs.pose.pose.position.z]), axis=0)
-    position_dict["prediction"] = np.append(args[0].get("prediction"), new_position, axis=0)
-
-    new_orientation = np.expand_dims(np.array([pwcs.pose.pose.orientation.x,
-                                               pwcs.pose.pose.orientation.y,
-                                               pwcs.pose.pose.orientation.z,
-                                               pwcs.pose.pose.orientation.w]), axis=0)
-    orientation_dict["prediction"] = np.append(args[1].get("prediction"), new_orientation, axis=0)
-
-    new_gt_position = np.expand_dims(np.array(trans), axis=0)
-    position_dict["gt"] = np.append(args[0].get("gt"), new_gt_position, axis=0)
-
-    new_gt_orientation = np.expand_dims(np.array(rot), axis=0)
-    orientation_dict["gt"] = np.append(args[1].get("gt"), new_gt_orientation, axis=0)
-
-    plot3D.plot(position_dict["prediction"][:, 0],
-                position_dict["prediction"][:, 1],
-                position_dict["prediction"][:, 2], label='predicted position')
-    plot3D.plot(position_dict["gt"][:, 0],
-                position_dict["gt"][:, 1],
-                position_dict["gt"][:, 2], label='gt position')
-    #ToDo: plot orientation
-    #ToDo: fix proble with non-displayed labels
-    pass
-
-
-if __name__ == "__main__":
-    try:
-        plotter()
-    except rospy.ROSInterruptException:
-        pass
